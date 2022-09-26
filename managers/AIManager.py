66f6451be246b1a624987352c61c5e9af6c8cae9
Yan Sidyakin <sidyakinian@gmail.com>
2022-09-26 10:44:16 -0700

    Call Cohere API from AIManager

--- managers/AIManager.py
+++ managers/AIManager.py
@@ -1,13 +1,20 @@
 from dotenv import load_dotenv
 import sys, os, base64, datetime, hashlib, hmac, requests, json
+import cohere
 
 load_dotenv()
 
 class AIManager(object):
     def __init__(self):
-        # Initialize API keys
-        pass
+        api_key = os.environ['COHERE_API_KEY']
+        self.co = cohere.Client(api_key)
 
     async def complete_prompt(self, inputs):
-        # Make API call to AI
-        return "true"
+        response = self.co.classify(
+            model=os.environ['COHERE_MODEL_ID'],
+            inputs=[inputs],
+            examples=[]
+        )
+        response_labels = response.classifications[0].labels
+        most_likely_label = max(response_labels, key = lambda k: response_labels.get(k).confidence)
+        return most_likely_label
