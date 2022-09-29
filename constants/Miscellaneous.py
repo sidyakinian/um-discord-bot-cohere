4ecc09104b12892b12b265ed8fc2474fb28e90e8
Yan Sidyakin <sidyakinian@gmail.com>
2022-09-29 06:20:20 -0700

    Transform into readme only

--- constants/Miscellaneous.py
+++
@@ -1,2 +0,0 @@
-class Miscellaneous(object):
-    list_of_commands = ['puzzles', 'play', 'hint', 'submit', 'giveup', 'quit', 'help', 'feedback', '_puzzle_data', '_space', '_crash', '_complete_prompt']
